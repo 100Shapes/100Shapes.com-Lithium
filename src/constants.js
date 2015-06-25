5678222952291496fe8df40c092d1020758ce1b9
Michele Memoli <michele@100shapes.com>
2015-06-25 23:00:38 +0100

    Successfully talk to (local) API

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export default ngModule => {
     ngModule
 
         // SMP settings
-        .constant('API_BASE_URL', 'http://localhost:8000/api/v1/pages/')
+        .constant('API_BASE_URL', 'http://localhost:8000/api/v1/')
 
         ;
 
--- src/states/blog/BlogContentService.js
+++ src/states/blog/BlogContentService.js
@@ -1,5 +1,6 @@
 export default ngModule => {
 
+    const url = require('url');
 
     ngModule.factory('BlogContentService', BlogContentService)
 
@@ -8,7 +9,28 @@ export default ngModule => {
         return {
 
             all() {
-                return 2;
+
+                const POSTS_LIST_URL = url.resolve(API_BASE_URL, 'pages/');
+
+                return $http.get(POSTS_LIST_URL,
+                    {
+                        params: {
+                            type: 'blog.BlogPost',
+                            fields: [
+                                'title',
+                                'thumbnail_url',
+                                'posted_at',
+                                'post_category'
+                            ].join(','),
+                            order: 'posted_at'
+                        }
+                    }
+                ).then(
+                    function (response) {
+                        return response.data.pages;
+                    }
+                );
+
             }
 
         };
