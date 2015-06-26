7efef2271b290a4b952e2ebebd42b530716a888a
Michele Memoli <michele@100shapes.com>
2015-06-26 13:15:06 +0100

    global-masthead styling

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,5 +1,6 @@
 export default ngModule => {
     
     require('./ohs-content-renderer')(ngModule);
+    require('./ohs-global-masthead')(ngModule);
 
 }
---
+++ src/shared/ohs-global-masthead/index.js
@@ -0,0 +1,25 @@
+module.exports = function(ngModule) {
+
+    require('./ohs-global-masthead.less');
+
+    ngModule
+        .directive('ohsGlobalMasthead', ohsGlobalMasthead);
+
+    function ohsGlobalMasthead() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: true,
+            controller: 'OhsGlobalMastheadCtrl as vm',
+            template: require('./ohs-global-masthead.html')
+        };
+    }
+
+    ngModule.controller('OhsGlobalMastheadCtrl', OhsGlobalMastheadCtrl);
+
+    function OhsGlobalMastheadCtrl(COMPANY_META) {
+        let vm = this;
+
+        vm.company = COMPANY_META;
+    }
+};
