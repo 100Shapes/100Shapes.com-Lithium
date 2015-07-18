8b0e4da8add043c7eee251ac61ad156db47644b5
Michele Memoli <michele@100shapes.com>
2015-07-18 11:57:47 +0100

    Made cards use flexbox #14

--- src/shared/ohs-off-canvas/index.js
+++ src/shared/ohs-off-canvas/index.js
@@ -1,15 +1,19 @@
 module.exports = function(ngModule) {
 
     require('./ohs-off-canvas.less');
+    const angular = require('angular');
 
     ngModule
         .factory('OffCanvas', OffCanvas);
 
-    function OffCanvas(cnOffCanvas) {
+    function OffCanvas(cnOffCanvas, $document) {
+
+        const nav = angular.element($document[0].querySelector('.ohs-global-masthead__off-canvas'));
 
         const offCanvas = cnOffCanvas({
             controller: 'OffCanvasCtrl',
             controllerAs: 'vm',
+            container: nav,
             containerClass: 'ohs-off-canvas--open',
             template: require('./ohs-off-canvas.html')
         });
