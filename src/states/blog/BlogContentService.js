ac8338055b9f422b6de8d7b7227a15a0cfcbefb7
Michele Memoli <michele@100shapes.com>
2015-08-01 13:01:13 +0100

    Migrated BlogContentServices to use the base contentFactory

--- src/states/blog/BlogContentService.js
+++ src/states/blog/BlogContentService.js
@@ -2,9 +2,22 @@ export default ngModule => {
 
     ngModule.factory('BlogContentService', BlogContentService);
 
-    function BlogContentService(ApiBaseService) {
+    function BlogContentService(ApiBaseService, AUTHORS) {
 
         let blogContentService = new ApiBaseService('blog');
+
+        blogContentService.one = function (slug) {
+
+            const baseClass = Object.getPrototypeOf(this);
+
+            return baseClass.one.call(this, slug).then(
+                (post) => {
+                    let author_slug = post.author;
+                    post.author = AUTHORS[author_slug];
+                    return post;
+                }
+            );
+        };
 
         return blogContentService;
     }
