3a6871f2e89f25a728a88e45214a385c5e840158
Michele Memoli <michele@100shapes.com>
2015-08-01 18:11:02 +0100

    Removed unused secondary-nav from service item page

--- src/states/services/item/index.js
+++ src/states/services/item/index.js
@@ -27,16 +27,11 @@ export default ngModule => {
 
     ngModule.controller('ServicesItemCtrl', ServicesItemCtrl);
 
-    function ServicesItemCtrl(service, COMPANY_META, $document, GlobalMastheadService, GLOBAL_MASTHEAD_THEMES) {
+    function ServicesItemCtrl(service, COMPANY_META, GlobalMastheadService, GLOBAL_MASTHEAD_THEMES) {
         let vm = this;
 
         vm.service = service;
         vm.company = COMPANY_META;
-
-        var aside = angular.element($document[0].querySelector('.service-page__secondary-nav'));
-        var wrapperFlush = angular.element($document[0].querySelector('.wrapper-flush'));
-
-        aside.css("left", wrapperFlush.prop('offsetLeft')+'px');
 
         GlobalMastheadService.theme = `${GLOBAL_MASTHEAD_THEMES.TRANSPARENT} ${GLOBAL_MASTHEAD_THEMES.BLUE}`;
     }
