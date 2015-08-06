2a52cb5f01aad44665538a9e1247b64a459cec7b
Paolo Memoli <paolo@100shapes.com>
2015-08-06 23:11:48 +0100

    added back in tidy up

--- update-content.sh
+++ update-content.sh
@@ -2,7 +2,7 @@
 echo "Clearing old Content from branch: ${BRANCH:-master}@${GIT_URL}"
 git clone --depth 1 -b ${BRANCH:-master} ${GIT_URL} /tmp/app
 cp -r /tmp/app/* /home/app/
-# rm -r /tmp/app
+rm -r /tmp/app
 cd /home/app/
 echo "Content Pulled. Running npm install"
 npm install
