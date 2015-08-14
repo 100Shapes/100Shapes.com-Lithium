69fa1ceb627947904d9cd69e33287ca167e0d528
Michele Memoli <michele@100shapes.com>
2015-08-15 00:31:37 +0100

    Refactored ohs-blogpost-callout to only accept a slug closes #30

--- src/shared/ohs-blogpost-callout/index.js
+++ src/shared/ohs-blogpost-callout/index.js
@@ -1,26 +1,49 @@
 module.exports = function(ngModule) {
 
     require('./ohs-blogpost-callout.less');
+    const angular = require('angular');
 
     ngModule
         .directive('ohsBlogpostCallout', ohsBlogpostCallout);
 
-    function ohsBlogpostCallout(DEFAULT_BLOGPOST_CALLOUT_CONFIG) {
+    function ohsBlogpostCallout(DEFAULT_BLOGPOST_CALLOUT_CONFIG, $parse) {
         return {
             restrict: 'E',
             replace: true,
             scope: {
-                model: '=',
-                message: '@?'
+                config: '='
             },
             template: require('./ohs-blogpost-callout.html'),
+            controller: 'OhsBlogpostCallout as vm',
+            bindToController: true,
             link($scope, elem, attrs) {
-                if (!attrs.message) {
-                    $scope.intro = DEFAULT_BLOGPOST_CALLOUT_CONFIG.message;
+
+                let default_config = angular.copy(DEFAULT_BLOGPOST_CALLOUT_CONFIG);
+
+                if (!attrs.config) {
+                    throw new Error(`Please specify a 'config' attribute.`);
                 }
+
+                const local_config = $parse(attrs.config)();
+
+                if (!local_config.slug) {
+                    throw new Error(`Please specify a 'config.slug' attribute.`);
+                }
+
+                $scope.config = angular.extend(default_config, local_config);
+
             }
         };
     }
 
+    ngModule.controller('OhsBlogpostCallout', OhsBlogpostCallout);
+
+    function OhsBlogpostCallout(BlogContentService) {
+        let vm = this;
+
+        BlogContentService.one(vm.config.slug).then((post) => {
+            vm.model = post;
+        });
+    }
 
 };
